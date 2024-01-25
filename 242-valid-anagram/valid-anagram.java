1211988ef02f52051f6dd1f0816fa4bf82f78ba8
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-01-25 23:03:48 +0530

    Time: 4 ms (67.83%) | Memory: 44.6 MB (13.67%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null || t == null || s.length() != t.length())
+            return false;
+        char sArr[] = s.toCharArray();
+        char tArr[] = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return Arrays.equals(sArr, tArr);
+    }
+}
