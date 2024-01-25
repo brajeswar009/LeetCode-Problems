129a3822895e3b61ce653e3f8233ce704c18480f
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-01-25 23:17:57 +0530

    Time: 2 ms (97.03%) | Memory: 43.3 MB (63.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,12 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // since only lowercase used created an to store count of letters
+        int count[] = new int[26];
+        // checking for edge cases
         if (s == null || t == null || s.length() != t.length())
             return false;
-        char sArr[] = s.toCharArray();
-        char tArr[] = t.toCharArray();
-        Arrays.sort(sArr);
-        Arrays.sort(tArr);
 
-        return Arrays.equals(sArr, tArr);
+        // converting string to char array and increasing the count of letters as per
+        // their position in
+        // alphabet order(to get alphabet order substracting from a) eg; h-a(104-97) = 7
+        // so count[7] +1 etc.
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        // converting string to char array and decresing the count of letters as per
+        // their position in alphabet order
+        for (char d : t.toCharArray()) {
+            count[d - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0)
+                return false;
+        }
+        return true;
     }
 }
