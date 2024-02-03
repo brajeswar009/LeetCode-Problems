9e16575c988e49a769b445f11c9197607c724408
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-03 15:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (66.55%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        boolean res = true;
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
+        for (int i = 0; i < rowLen - 1; i++) {
+            for (int j = 0; j < colLen - 1; j++) {
+                if (matrix[i][j] == matrix[i + 1][j + 1]) {
+                    continue;
+                } else {
+                    res = false;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
