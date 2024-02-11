6a90dfdd600d4e9eddd41b4b4ec370dd0bcc02ee
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-11 22:40:30 +0530

    Time: 1 ms (88.85%) | Memory: 40.9 MB (43.02%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,23 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-        int left = 0;
-        int right = x;
+        // binary search approach where we find the mid to change the target range
+        // using long to tackle huge numbers (O(log n)) time complexity
+        int left = 0, right = x;
+        int res = 0;
         int mid = 0;
-        int probableAns = 0;
+        while (left <= right) {
+            // to find the perfect mid without overflow (simiar to left+right/2)
+            mid = left + (right - left) / 2;
 
-        while(left <= right){
-            mid = left + (right-left)/2;
-            if((long)mid*mid <= (long)x){
-                probableAns = mid;
-                // let's see if we can find a bigger num
-                left = mid+1;
-            }
-            else if((long)mid*mid > (long)x){
-                right = mid-1;
-            }
+            // if the mid^2 is smaller than x then the desired number will be in the right 
+            // half of the mid so shifting the left pointer
+            if ((long)mid * mid <= (long)x) {
+                res = mid;
+                left = mid + 1;
+            } 
+            // if the mid^2 is bigger than x then the desired number 
+            // will be in the left half of the mid so shifting the right pointer
+            else if ((long)mid * mid > (long)x) {
+                right = mid - 1;
+            } 
         }
-
-        return probableAns;
-
+        return res;
     }
 }
