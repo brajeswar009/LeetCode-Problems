ab5a7c244cc7ce5a5e320661f38f17a6e1e00dc3
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-14 07:32:24 +0530

    Update running-sum-of-1d-array.java

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -6,3 +6,9 @@ class Solution {
         return nums;
     }
 }
+/*
+Time complexity - O(N)
+Space Complexity - O(1)
+Logic is, as we can observe from the question that, a particular index's value is the sum of the previous index and the current index
+so using this logic to add the index values to its previous one.
+*/
