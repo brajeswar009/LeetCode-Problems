819ab5600afcc97232920ee39081cc90b6a06636
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-14 19:14:23 +0530

    Time: 1 ms (65.88%) | Memory: 41.4 MB (91.52%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,6 +1,26 @@
 class Solution {
     public int maximumDifference(int[] nums) {
-        int res = 0;
+        int ans = -1, minNumTillNow = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (minNumTillNow < num) {
+                ans = Math.max(ans, num - minNumTillNow);
+            }
+            minNumTillNow = Math.min(minNumTillNow, num);
+        }
+        return ans;
+    }
+}
+
+/*Logic is:- to get the maximum difference then we have to subtract
+ * the highest number from lowest number with keeping in mind that 
+ * nums[i]<nums[j] as given in question. using this logic here
+ O(N)
+ */
+
+
+
+/* BruteForce O(N^2)
+int res = 0;
         int maxDiff = 0;
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -14,5 +34,4 @@ class Solution {
             return -1;
         else
             return maxDiff;
-    }
-}
+*/
