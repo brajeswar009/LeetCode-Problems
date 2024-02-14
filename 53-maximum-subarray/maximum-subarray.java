bb09abf32d162a6b8162815bc0d16f0ba0668f09
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-14 16:59:35 +0530

    Update maximum-subarray.java

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,8 +5,16 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
             max = max > sum ? max : sum;
+            //If less than 0, then the current window can be discarded
+            // because it is not going to be useful to find the maximum sum subarray
             sum = sum > 0 ? sum : 0;
         }
         return max;
     }
 }
+
+/*
+This is called Kadane's algorithm.
+Time Complexity - O(N), and space complexity - O(1)
+
+*/
