ac04eb4149dac19b80117a382b5d730924da78d9
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-20 18:44:22 +0530

    Time: 1 ms (33.12%) | Memory: 50.1 MB (6.54%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,13 +1,27 @@
 class Solution {
     public void reverseString(char[] s) {
-        int start = 0;
-        int end = s.length-1;
-        while(start<=end){
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
-        }
+        recursiveApproach(s, 0, s.length - 1);
+    }
+    private void recursiveApproach(char[] s, int left, int right) {
+        if (left >= right) return;
+        char tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        recursiveApproach(s, left+1, right-1);
     }
 }
+
+
+/*
+Iterative Approach:- TC- O(N), SC - O(1)
+int start = 0;
+int end = s.length-1;
+while(start<=end){
+char temp = s[start];
+s[start] = s[end];
+s[end] = temp;
+start++;
+end--;
+}
+
+*/
