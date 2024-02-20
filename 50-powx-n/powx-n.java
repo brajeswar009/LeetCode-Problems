8d3a9c435e50e696ff52a45888a0cc0069687c62
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2024-02-20 17:52:53 +0530

    Time: 1 ms (11.77%) | Memory: 41.7 MB (81.09%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,11 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
+        //2^-3 = 1/2^3 so dividing the result by 1 if n is -ve
         if(n < 0)
             return 1/myPowFinder(x, Math.abs(n));
         return myPowFinder(x, n);
     }
     public double myPowFinder(double x, int n){
         if(n == 0) return 1;
+        // x^n = x * x^n-1 (one way to find power)
+        // if n is even, then x^n = x^n/2 * x^n/2
+        // if n is odd, then x^n = x^n/2 * x^n/2 * x 
         double temp = myPowFinder(x, n/2);
         if(n % 2 == 0)
             return temp * temp;
